ccf14d0c10d1dad5e544050f9dffe252a3fa4ebc
Adalberto F García Espinosa <adalberto.garcia@iaware.pro>
2025-06-19 21:43:41 -0600

    Finish module 226

--- 001-key-data-extraction.py
+++ 001-key-data-extraction.py
@@ -5,7 +5,7 @@ openai_api_key = os.environ["OPENAI_API_KEY"]
 
 from langchain_openai import ChatOpenAI
 
-llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+llm = ChatOpenAI(model="gpt-4o-mini")
 
 from typing import Optional
 
